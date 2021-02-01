95e793e2d8f4a85c23f302ba8bf2ef98015a2eb8
Gustavo Lopez <5049755+slave-of-the-code@users.noreply.github.com>
2021-02-01 15:03:46 -0300

    Update Menu.js

--- src/components/menu/Menu.js
+++ src/components/menu/Menu.js
@@ -6,18 +6,10 @@ import "./Menu.scss";
 export const Menu = () => {
   return (
     <nav className="menu">
-      <NavLink exact to="/" activeClassName="active">
-        Home
-      </NavLink>
-      <NavLink to="/GameList" activeClassName="active">
-        Game List
-      </NavLink>
-      <NavLink to="/ShipSetting" activeClassName="active">
-        Ship Setting
-      </NavLink>
-      <NavLink to="/BoardSetting/:id" activeClassName="active">
-        Board Setting
-      </NavLink>
+      <NavLink exact to="/" activeClassName="active">Home</NavLink>
+      <NavLink exact to="/GameList" activeClassName="active">Game List</NavLink>
+      <NavLink exact to="/ShipSetting" activeClassName="active">Ship Setting</NavLink>
+      <NavLink exact to="/BoardSetting/:id" activeClassName="active">Board Setting</NavLink>
     </nav>
   );
 };
